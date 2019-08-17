9aec74a488a796e260bd40a327225cf70b6ca10d
CGS-Matthew-Purcell <matthew.purcell@cgs.act.edu.au>
2019-08-17 13:08:41 +1000

    Layout and counting time working

--- meos-results-screens/src/components/ResultsTable.vue
+++ meos-results-screens/src/components/ResultsTable.vue
@@ -1,49 +1,56 @@
 <template>
 
-	<div>
+	<div v-if="pages[pageNum]" class="columns">
 
-		<div v-for="cls in resultsResponse.cmpResults" v-bind:key="cls.clsId">
+		<div
+			v-for="(column, columnI) of pages[pageNum]"
+			:key="columnI"
+			class="column"
+			:style="{ marginLeft: (columnI === 0 ? 0 : columnGap) + 'px' }"
+		>
 
-			<table>
+			<table
+				v-for="(results, resultsI) of column"
+				:key="resultsI"
+			>
 
 				<tbody>
 
-					<tr class="headingRow">
-						<th class="className" colspan="5">{{ cls.clsName }}</th>
-						<th colspan="3" v-for="n in cls.radioCount" v-bind:key="n"><font-awesome-icon icon="broadcast-tower" /><sub>{{ n }}</sub></th>
+					<tr class="headingRow" :style="{ height: rowHeight * 2 + 'px' }">
+						<th class="className" colspan="5">{{ results.cls.clsName }} {{ results.continued ? '(Cont)' : '' }}</th>
+						<th colspan="3" v-for="n in results.cls.radioCount" :key="n"><font-awesome-icon icon="broadcast-tower" /><sub>{{ n }}</sub></th>
 					</tr>
-				
-					<tr v-for="result in cls.clsResults" v-bind:key="result.id">
 
-						<td v-if="result.status == 0" class="col-overallRank"><font-awesome-icon v-bind:icon="statusZero(result)" /></td>
-						<td v-else-if="result.status == 1" class="col-overallRank">{{ result.finishRank }}</td>
-						<td v-else-if="result.status == 3" class="col-overallRank">MP</td>
-						<td v-else-if="result.status == 4" class="col-overallRank">DNF</td>
-						<td v-else-if="result.status == 5" class="col-overallRank">DQ</td>
-						<td v-else-if="result.status == 6" class="col-overallRank">OT</td>
-						<td v-else-if="result.status == 7" class="col-overallRank">DNS</td>
-						<td v-else-if="result.status == 8" class="col-overallRank">CNL</td>
-						<td v-else-if="result.status == 9" class="col-overallRank">NP</td>
-						<td v-else class="col-overallRank"></td>
+					<tr v-for="result of results.results" :key="result.id" :style="{ height: rowHeight + 'px' }">
 
-						<td class="col-competitor">{{ result.competitor }}</td>
-						<td class="col-club">GS A</td>
+						<td class="col-overallRank" :style="{ width: colOverallRank + 'px' }">
+							<font-awesome-icon v-if="result.status == 0" :icon="statusZero(result)" />
+							<template v-else-if="result.status == 1">{{ result.finishRank }}</template>
+							<template v-else>{{ statusToRank[result.status] }}</template>
+						</td>
 
-						<td v-if="result.finishTime == null" class="col-elapsedTime">{{ formatAbsoluteTime(calculateElapsedTime(result.startTime)) }}</td>
-						<td v-else class="col-elapsedTime">{{ result.finishTime }}</td>
+						<td class="col-competitor" :style="{ width: colCompetitor + 'px' }">{{ result.competitor }}</td>
+						<td class="col-club" :style="{ width: colClub + 'px' }">GS A</td>
 
-						<td class="col-elapsedDiff">{{ result.finishDiff }}</td>
+						<td class="col-elapsedTime" :style="{ width: colElapsedTime + 'px' }">
+							<template v-if="result.finishTime == null">{{ (calculateElapsedTime(result.startTime) / 10) | formatAbsoluteTime }}</template>
+							<template v-else>{{ result.finishTime }}</template>
+						</td>
+
+						<td class="col-elapsedDiff" :style="{ width: colElapsedDiff + 'px' }">{{ result.finishDiff }}</td>
 
 						<!-- we need to use i (index) rather than n (value) so we start at zero-->
-						<template v-for="(n, i) in cls.radioCount">
+						<template v-for="(n, i) in results.cls.radioCount">
 
-							<td class="col-radioTime" v-bind:key="result.id + '_' + i + '_time'">{{ result.radios[i].time }}</td>
+							<td class="col-radioTime" :key="result.id + '_' + i + '_time'" :style="{ width: colRadioTime + 'px' }">{{ result.radios[i].time }}</td>
 
 							<!-- if no radio punch then print no brackets-->
-							<td class="col-radioRank" v-if="result.radios[i].time == null" v-bind:key="result.id + '_' + i + '_rank'"></td>
-							<td class="col-radioRank" v-else v-bind:key="result.id + '_' + i + '_rank'">({{ result.radios[i].rank }})</td>
+							<td class="col-radioRank" :key="result.id + '_' + i + '_rank'" :style="{ width: colRadioRank + 'px' }">
+								<template v-if="result.radios[i].time == null"></template>
+								<template v-else>({{ result.radios[i].rank }})</template>
+							</td>
 
-							<td class="col-radioDiff" v-bind:key="result.id + '_radio' + i + '_diff'">{{ result.radios[i].diff }}</td>
+							<td class="col-radioDiff" :key="result.id + '_radio' + i + '_diff'" :style="{ width: colRadioDiff + 'px' }">{{ result.radios[i].diff }}</td>
 
 						</template>
 
@@ -59,8 +66,6 @@
 
 </template>
 
-
-
 <style>
 
 @import url('https://fonts.googleapis.com/css?family=Roboto');
@@ -75,6 +80,8 @@ body {
 
 table {
 	border-collapse: collapse;
+	white-space: nowrap;
+	table-layout: fixed;
 }
 
 table tr td {
@@ -82,14 +89,15 @@ table tr td {
 	vertical-align: bottom;
 }
 
-tr.headingRow {
-	height: 38px; /* IMPORTANT: Heading row height */
+td, th {
+	padding: 0 3px;
+	box-sizing: border-box;
 }
 
 th {
 	border-bottom: 1px solid #777;
 	vertical-align: bottom;
-	padding-bottom: 8px;
+	padding-bottom: 10px;
 }
 
 th.className {
@@ -99,19 +107,16 @@ th.className {
 }
 
 td {
-	height: 22px; /* IMPORTANT: Result row height */
 	border-bottom: 1px solid #777;
 }
 
 td.col-overallRank {
 	text-align: center;
-	width: 40px; /* IMPORTANT: Overall rank column width */
 	font-size: 13px;
 	padding-bottom: 4px;
 }
 
 td.col-competitor {
-	width: 180px; /* IMPORTANT: Competitor column width */
 	max-width: 180px;
 	white-space: nowrap;
 	overflow: hidden;
@@ -119,45 +124,39 @@ td.col-competitor {
 }
 
 td.col-club {
-	width: 50px; /* IMPORTANT: Club column width */
 }
 
 td.col-elapsedTime {
 	text-align: right;
-	width: 65px; /* IMPORTANT: Elapsed time column width */
 }
 
 td.col-elapsedDiff {
-	width: 45px; /* IMPORTANT: Elapsed time diff column width */
 	text-align: left;
 	font-size: 12px;
-	padding-left: 8px; /* IMPORTANT: Add padding onto the width calcs below! */
-	padding-bottom: 4px;
 }
 
 td.col-radioTime {
 	text-align: right;
-	width: 65px; /* IMPORTANT: Radio time column width */
 }
 
 td.col-radioRank {
 	font-size: 12px;
-	padding-left: 4px; /* IMPORTANT: Add padding onto the width calcs below! */
-	padding-bottom: 4px;
-	width: 25px; /* IMPORTANT: Radio rank column width */
 }
 
 td.col-radioDiff {
-	width: 47px; /* IMPORTANT: Radio time diff column width */
 	text-align: left;
 	font-size: 12px;
-	padding-left: 8px; /* IMPORTANT: Add padding onto the width calcs below! */
-	padding-bottom: 4px;
+}
+
+.columns {
+	display: flex;
+}
+
+.column {
+	flex: 0 0 auto;
 }
 
 </style>
-
-
 
 <script>
 
@@ -167,29 +166,145 @@ td.col-radioDiff {
 
 		data() {
 			return {
-				resultsResponse: {}
+				now: new Date(),
+				pageNum: +new URLSearchParams(window.location.search).get('page') || 0,
+				resultsResponse: null,
+
+				windowWidth: 0,
+				windowHeight: 0,
+				rowHeight: 22,
+				headerRowHeight: 44,
+				columnGap: 20,
+
+				// Column widths
+				colOverallRank: 40,
+				colCompetitor: 180,
+				colClub: 50,
+				colElapsedTime: 65,
+				colElapsedDiff: 45,
+				colRadioTime: 65,
+				colRadioRank: 30,
+				colRadioDiff: 47,
+
+				statusToRank: {
+					3: 'MP',
+					4: 'DNF',
+					5: 'DQ',
+					6: 'OT',
+					7: 'DNS',
+					8: 'CNL',
+					9: 'NP',
+				}
 			}
 		},
 
-		async created () {
-			this.refreshResults()
+		computed: {
+			// Splits the data into "pages" such that all of the data within each page
+			// will fit as efficiently as possible onto the page
+			pages() {
+				const {
+					resultsResponse,
+					windowWidth,
+					windowHeight,
+					rowHeight,
+					headerRowHeight,
+					columnGap,
+					colOverallRank,
+					colCompetitor,
+					colClub,
+					colElapsedTime,
+					colElapsedDiff,
+					colRadioTime,
+					colRadioRank,
+					colRadioDiff,
+				} = this
+
+				if (!resultsResponse) {
+					return []
+				}
+
+				const pages = []
+				let page = null
+				let column = null
+				let results = null
+				let lastCls = null
+				let cls = null
+				let tableWidth = 0
+				let columnWidth = 0
+				let columnHeight = 0
+				let overallWidth = 0
+
+				const tableBaseWidth = colOverallRank + colCompetitor + colClub + colElapsedTime + colElapsedDiff
+				const tableRadioWidth = colRadioTime + colRadioRank + colRadioDiff
+
+				const fit = height => {
+					const additionalWidth = Math.max(0, tableWidth - columnWidth)
+					const overflowH = overallWidth + additionalWidth > windowWidth
+					const overflowV = columnHeight + height > windowHeight
+
+					// Do we need another page?
+					if (!page || (overflowH && page.length > 1)) {
+						page = []
+						pages.push(page)
+						column = null
+						results = null
+						columnWidth = 0
+						columnHeight = 0
+						overallWidth = 0
+						fit(height)
+						return
+					}
+
+					// Do we need another column?
+					if (!column || overflowV) {
+						column = []
+						page.push(column)
+						results = null
+						columnWidth = tableWidth
+						columnHeight = 0
+						overallWidth += tableWidth + (page.length === 1 ? 0 : columnGap)
+						fit(height)
+						return
+					}
+
+					// Do we need another results section?
+					if (!results) {
+						results = []
+						column.push({
+							cls,
+							results,
+							continued: lastCls === cls,
+						})
+						lastCls = cls
+						columnHeight += headerRowHeight
+						fit(height)
+						return
+					}
+
+					columnWidth += additionalWidth
+					columnHeight += height
+				}
+
+				for (cls of resultsResponse.cmpResults) {
+					tableWidth = tableBaseWidth + tableRadioWidth * cls.radioCount
+					results = null
+
+					for (const result of cls.clsResults) {
+						fit(rowHeight)
+						results.push(result)
+					}
+				}
+
+				return pages
+			},
 		},
 
-		methods: {
-
-			async refreshResults () {
-				this.resultsResponse = await meosResultsApi.getResults()
-				// eslint-disable-next-line
-				console.log(this.resultsResponse)
-			},
-
-			// Formats a time in 10ths of seconds into h:mm:ss or mm:ss
-			formatAbsoluteTime(t) {
-
-				if (t) {	
+		filters: {
+			formatAbsoluteTime: function(t) {
+				
+				if (t) {
 
 					var h, m, s;
-					t = t/10; // convert from 10ths of seconds into seconds
 
 					if (t > 3600) {
 						h = Math.floor(t/3600).toString();
@@ -208,19 +323,60 @@ td.col-radioDiff {
 				return null;
 
 			},
+		},
+
+		created () {
+			window.addEventListener('resize', () => this.updateWindowSize())
+			this.updateWindowSize()
+			this.refreshResults()
+
+			setInterval(() => this.now = new Date(), 1000)
+
+			const updateLoop = () => {
+				const nowMs = +new Date()
+				const updateIntervalMs = 5000;
+				const delay = Math.floor(nowMs / 1000) * 1000 - nowMs + updateIntervalMs
+
+				setTimeout(() => {
+					this.refreshResults()
+					updateLoop()
+
+				}, delay)
+			}
+
+			updateLoop()
+		},
+
+		methods: {
+
+			updateWindowSize() {
+				this.windowWidth = window.innerWidth
+				this.windowHeight = window.innerHeight
+			},
+
+			async refreshResults () {
+				this.resultsResponse = await meosResultsApi.getResults()
+			},
 
 			// Calculates the current elapsed time for a competitor, based on their startTime
 			calculateElapsedTime(startTime) {
-
 				// Time of day in 10ths of seconds
-				var dt = new Date();
-				var currentTimeSecs = (dt.getSeconds() + (60 * dt.getMinutes()) + (60 * 60 * dt.getHours())) * 10;
+				const { now } = this;
+				const currentTimeSecs = (now.getSeconds() + (60 * now.getMinutes()) + (60 * 60 * now.getHours())) * 10;
 
 				// Calculate elapsed running time
-				var elapsedRunningTime = currentTimeSecs - startTime;
+				const elapsedRunningTime = currentTimeSecs - startTime;
 
-				// Return the result
-				return elapsedRunningTime;
+				// Check that it's positive
+				if (elapsedRunningTime >= 0) {
+
+					// Return the result
+					return elapsedRunningTime;
+
+				}
+
+				// Otherwise, return null
+				return null;
 
 			},
 
@@ -230,19 +386,19 @@ td.col-radioDiff {
 				// Calculate their elapsed time
 				var elapsedRunningTime = this.calculateElapsedTime(resultObject.startTime);
 
-				// They have started...
-				if (elapsedRunningTime >= 0) {
-
-					// Set the icon to a running man
-					return `running`;							
-
-				}
-
-				// They have not yet started...
-				else {
+				// They have not started
+				if (elapsedRunningTime == null) {
 
 					// Set the icon to three dots (waiting...)
 					return `ellipsis-h`;
+
+				}
+
+				// They have started...
+				else {
+
+					// Set the icon to a running man
+					return `running`;
 
 				}
 
